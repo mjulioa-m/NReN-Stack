f44cb6c8063c6615cc53bf2d0f6d5eecf8667f7b
miguel <miguelantoniojuliodiaz@gmail.com>
2025-11-07 13:20:25 -0500

    Se arreglo un pequeño gran problema con el cloudinary jeje

--- bellafly-api/src/cloudinary/cloudinary.provider.ts
+++ bellafly-api/src/cloudinary/cloudinary.provider.ts
@@ -1,21 +1,23 @@
 import { Provider } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { v2 as cloudinary } from 'cloudinary'; // <-- 1. Importa el SDK de Cloudinary
+import { v2 as cloudinary } from 'cloudinary'; // Importa el SDK
 
-// 2. Este es el "Token" que usaremos para inyectarlo en otros servicios
 export const CLOUDINARY = 'Cloudinary';
 
-// 3. Este es el "Provider" personalizado
 export const CloudinaryProvider: Provider = {
-  provide: CLOUDINARY, // El nombre/token del provider
+  provide: CLOUDINARY,
   useFactory: (configService: ConfigService) => {
-    // 4. Esta es la "fábrica" que se ejecuta al inicio
-    //    Lee las claves del .env y configura el SDK
-    return cloudinary.config({
+    // --- AQUÍ ESTÁ LA CORRECCIÓN ---
+
+    // 1. Llama a la configuración (esto no devuelve nada)
+    cloudinary.config({
       cloud_name: configService.get<string>('CLOUDINARY_CLOUD_NAME'),
       api_key: configService.get<string>('CLOUDINARY_API_KEY'),
       api_secret: configService.get<string>('CLOUDINARY_API_SECRET'),
     });
+
+    // 2. Devuelve el SDK (el objeto 'cloudinary' importado)
+    return cloudinary;
   },
-  inject: [ConfigService], // 5. Le decimos a NestJS que inyecte el ConfigService
+  inject: [ConfigService],
 };
